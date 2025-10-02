7171f90a6402b3a0c2e191fc77ff3191142142c2
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2025-10-02 23:43:48 +0200

    remove open api and make explicit build commands

--- Time.Api/Program.cs
+++ Time.Api/Program.cs
@@ -1,14 +1,6 @@
-using Scalar.AspNetCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-app.MapOpenApi();
-
-app.MapScalarApiReference();
 
 app.UseHttpsRedirection();
 
